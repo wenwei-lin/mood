60e838c3360e31ff4c023abeba05d36fbe82aad0
Wenwei Lin <wenwei66.lin@gmail.com>
2024-03-21 10:47:56 +0800

    create user in database

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
+import { prisma } from "@/utils/db";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  await CreateNewUser();
   return <p>Hi there~</p>;
 }
+
+const CreateNewUser = async () => {
+  const user = await currentUser();
+  console.log(user);
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect("/journal");
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  // fix redirect url not working
+  let getStartUrl = userId ? "/journal" : "/new-user";
+
   return (
     // Use 'w-screen h-screen' to make the div take up the entire screen.
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
@@ -12,7 +18,7 @@ export default function Home() {
           you have to do is be honest.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={getStartUrl}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
